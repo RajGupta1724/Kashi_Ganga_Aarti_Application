7e93b018516e9fbf4bc4922f0b8103bdfc555077
apnacollege <rajgupta1782005@gmail.com>
2026-03-22 01:30:25 +0530

    Simplify kashi_ganga/urls.py to standard Django root URL config

--- kashi_ganga/urls.py
+++ kashi_ganga/urls.py
@@ -1,18 +1,9 @@
 from django.contrib import admin
-
-from django.urls import path, include, re_path
-from django.conf import settings
-from django.conf.urls.static import static
-from django.views.static import serve as static_serve
-
-admin.site.site_header = "Kashi Ganga Admin Panel"
-admin.site.site_title = "Kashi Ganga"
-admin.site.index_title = "Manage Bookings & Content"
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # 👇 This line connects your entire app
     path('', include('main.urls')),
-] + [
-    re_path(r'^sitemap\.xml$', static_serve, {'path': 'sitemap.xml', 'document_root': settings.STATIC_ROOT}, name='sitemap'),
-    re_path(r'^robots\.txt$', static_serve, {'path': 'robots.txt', 'document_root': settings.STATIC_ROOT}, name='robots'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
