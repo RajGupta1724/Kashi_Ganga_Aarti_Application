c64d4749f01cbf094f92f250f37c89074da2edca
apnacollege <rajgupta1782005@gmail.com>
2026-05-30 18:00:59 +0530

    Add URL names for sitemap and robots views

--- kashi_ganga/urls.py
+++ kashi_ganga/urls.py
@@ -6,7 +6,7 @@ from main.views import robots_txt, sitemap_xml
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('robots.txt', robots_txt),
-    path('sitemap.xml', sitemap_xml),
+    path('robots.txt', robots_txt, name='robots'),
+    path('sitemap.xml', sitemap_xml, name='sitemap'),
     path('', include('main.urls')),
 ]
